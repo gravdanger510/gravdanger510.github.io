0bdf84dc25a5adef9777b06b3ffd749aae72b754
gravdanger510e <gravdanger510@gmail.com>
2016-04-18 12:26:10 -0400

    added shit davon says

---
+++ shitdavonsays/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+	<title></title>
+
+
+	<!--Load Random Stylesheet from css directory -->
+	<?php
+		$dir = 'https://dl.dropbox.com/sh/dbye05ojdd0rih6/AABYn2iDF-M3UuUD0CLbSETza/*';
+		$styles = array();
+		foreach(glob($dir) as $file){
+			if(!is_dir($file)){
+				array_push($styles, $file);
+				
+			}
+		}
+		$rand =  rand(0, count($styles) - 1);
+		
+		echo '<link rel="stylesheet" type="text/css" href="' . $styles[$rand] . '">';
+
+	?>
+	<link rel="stylesheet" type="text/css" href="https://dl.dropbox.com/sh/dbye05ojdd0rih6/AABYn2iDF-M3UuUD0CLbSETza/nature.css">
+
+</head>
+<body>
+
+	<h1 id="davon"></h1>
+
+
+<script type="text/javascript">
+	var tweets = [];
+	//Snag last 30 the tweets from @shitdavonsays
+	$.getJSON('tweets_json.php?count=30&screen_name=shitdavonsays', function(data){
+		var dataNum = data.length;
+		$.each(data, function(index, value){
+			tweets.push(value.text);
+			if (tweets.length === dataNum) {
+				//channel Davon's mind after all the data has loaded
+				newQuote();
+			}
+		});
+
+	});	
+	//Gimmie a random tweet
+	var newQuote = function(){
+		console.log("running");
+		var rand = Math.floor(Math.random() * tweets.length - 1);
+		$("#davon").text(tweets[rand]);
+	}
+	
+	
+</script>
+</body>
+</html>
